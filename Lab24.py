86e12351a886d8919f5b8f74f2eedc69722e75e2
Jed Rembold <jedediah.r@gmail.com>
2020-04-06 12:14:14 -0700

    Initial commit of instructions

---
+++ Lab24.py
@@ -0,0 +1,24 @@
+#############################################
+# Name: 
+# Collaborators(?):
+#############################################
+
+# You can go ahead and insert your class definitions below!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
+    # You can add any tests you want to run down here!
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Lab24
+# Lab 24: Plane ol' Carriers
+
+There are a lot of little classes to be written for today's lab, but many of them (and the methods within) are quite simple! See the posted lab for full instructions.
+
+I am going to try to get some tests working here, but we'll see how many I can get written in time!
