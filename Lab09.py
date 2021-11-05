7898e7ba9c6d2154cf135b09802b841f5b7f8767
Jed Rembold <jedediah.r@gmail.com>
2021-11-05 00:52:57 -0700

    Update lab number and tweak docstrings

--- Lab09.py
+++ Lab09.py
@@ -1,7 +1,8 @@
-#############################################
-# Name: 
-#############################################
+# Lab09.py
 
+""" Playing around with inheritance and class definitions in the context of different
+sorts of vehicles.
+"""
 
 class Vehicle:
     """ Generic class to store information about any vehicle. """
@@ -13,15 +14,25 @@ class Vehicle:
         return f"Vehicle({self.terrain},{self.weight})"
 
     def get_terrain(self):
+        """ Gets the terrain the vehicle traverses. """
         return self.terrain
 
     def set_terrain(self, new_terrain):
+        """ Set a new terrain type for the vehicle.
+        Inputs:
+            new_terrain (str): The new terrain type
+        """
         self.terrain = new_terrain
 
     def get_weight(self):
+        """ Get the weight of the vehicle. """
         return self.weight
 
     def set_weight(self, new_weight):
+        """ Set a new weight for the vehicle. 
+        Inputs:
+            new_weight (float): The new vehicle weight
+        """
         self.weight = new_weight
 
 # You can go ahead and insert your class definitions below!
@@ -39,4 +50,4 @@ class Vehicle:
 
 if __name__ == '__main__':
     pass
-    # You can add any tests you want to run down here!
+    # You can add any tests you want to run down here! And you should absolutely do so!
